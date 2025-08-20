562bf4f8896fc9a8a7594d9242f6537f877945b6
Abielon <lushen.abdiel@gmail.com>
2025-08-20 17:59:29 -0500

    1.40

--- docs/FighterJetSM-HTML5-Shipping.UE4.js
+++ docs/FighterJetSM-HTML5-Shipping.UE4.js
@@ -75,7 +75,7 @@ var Module = {
 	assetDownloadProgress: {}, // Track how many bytes of each needed asset has been downloaded so far.
 
 	UE4_indexedDBName: 'UE4_assetDatabase_FighterJetSM', // this should be an ascii ID string without special characters that is unique to the project that is being packaged
-	UE4_indexedDBVersion: 202508202136, // Bump this number to invalidate existing IDB storages in browsers.
+	UE4_indexedDBVersion: 202508202256, // Bump this number to invalidate existing IDB storages in browsers.
 };
 
 
@@ -312,7 +312,7 @@ function detectWebGL() {
 // Canvas scaling mode should be set to one of: 1=STRETCH, 2=ASPECT, or 3=FIXED.
 // This dictates how the canvas size changes when the browser window is resized
 // by dragging from the corner.
-var canvasWindowedScaleMode = 2 /*ASPECT*/;
+var canvasWindowedScaleMode = 1 /*STRETCH*/;
 
 // High DPI setting configures whether to match the canvas size 1:1 with
 // the physical pixels on the screen.
