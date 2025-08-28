ca2c9c42c0dbb7793fb53c479f556a38a8d10cfb
Abielon <lushen.abdiel@gmail.com>
2025-08-28 13:22:02 -0500

    1.57

--- docs/FighterJetSM-HTML5-Shipping.UE4.js
+++ docs/FighterJetSM-HTML5-Shipping.UE4.js
@@ -75,7 +75,7 @@ var Module = {
 	assetDownloadProgress: {}, // Track how many bytes of each needed asset has been downloaded so far.
 
 	UE4_indexedDBName: 'UE4_assetDatabase_FighterJetSM', // this should be an ascii ID string without special characters that is unique to the project that is being packaged
-	UE4_indexedDBVersion: 202508281731, // Bump this number to invalidate existing IDB storages in browsers.
+	UE4_indexedDBVersion: 202508281817, // Bump this number to invalidate existing IDB storages in browsers.
 };
 
 
